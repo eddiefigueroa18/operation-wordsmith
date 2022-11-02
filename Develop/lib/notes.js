2e96bd7a41749702425e59eff7a0cae0b3e67912
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-11-01 20:55:26 -0700

    Added create and delete functions in notes.js

---
+++ Develop/lib/notes.js
@@ -0,0 +1,39 @@
+//Node Modules
+const fs = require("fs");
+const path = require("path");
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//CREATE NOTES FUNCTION
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    notesArray.push(newNote);
+    //write the data into the "db.json" file
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"), 
+    JSON.stringify({notes: notesArray}, null, 2));
+    return newNote;
+};
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//DELETE NOTES FUNCTION
+function deleteNewNote(id, notesArray) {
+    const deleteNote = parseInt(id);
+    notesArray.splice(deleteNote, 1);
+    //Loop for re-writing indices for the remaining notes.
+    for (let i = deleteNote; i < notesArray.length; i++) {
+        notesArray[i].id = i.toString();
+    };
+
+    fs.writeFileSync(path.join(__dirname, "../db/db.json"),
+    JSON.stringify({notes: notesArray}, null, 2), err => {
+            if (err) {
+                throw err;
+            }
+        });
+};
+
+module.exports = {
+    createNewNote,
+    deleteNewNote
+};
